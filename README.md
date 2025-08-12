9883c008ab405fd2a9352ab167b894904f8aac09
Hossein <hosseinyadegarnia1391@gmail.com>
2025-08-12 15:05:59 +0330

    Add index.php

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # PHP Authentication
+
+[Favicon by Uniconlabs](https://www.flaticon.com/free-icon/two-factor-authentication_14247437?term=authentication&page=1&position=8&origin=tag&related_id=14247437)
--- index.php
+++ index.php
@@ -1,1 +1,28 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="styles/font.css" />
+    <link rel="stylesheet" href="styles/index.css" />
+    <title>PHP Authentication</title>
+</head>
+<body>
+
+<div class="menu">
+    <a href="https://github.com/hosseinyn/PHP-Authentication">GitHub</a>
+    Created by PHP , MySQL
+</div>
+
+<h1 style="margin-top: 50px">PHP Authentication</h1>
+
+<div style="text-align: center;"><img src="assets/icon.png" alt="icon" /></div>
+
+<p>There are your options : </p>
+
+<div class="options-button">
+    <a href="pages/login.php"><button>Login</button></a>
+    <a href="pages/register.php"><button>Register</button></a>
+</div>
+
+</body>
+</html>
