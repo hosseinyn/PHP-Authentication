09ca37dc4c593aa7bf250be85a1ecc5c66436ee3
Hossein <hosseinyadegarnia1391@gmail.com>
2025-08-12 15:59:37 +0330

    Add dashboard

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -1,1 +1,36 @@
 <?php
+
+session_start();
+
+if (!isset($_SESSION["loggedIn"]) || $_SESSION["loggedIn"] !== true) {
+    header("location: login.php");
+    exit;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../styles/font.css" />
+    <link rel="stylesheet" href="../styles/dashboard.css" />
+    <link rel="icon" href="../assets/favicon.ico" />
+    <title>PHP Dashboard</title>
+</head>
+<body>
+
+<?php
+echo "<h1 style='margin-top: 50px;'>Welecome $_SESSION[username] ! </h1>";
+?>
+
+<p>What to do : </p>
+
+<div class="options-button">
+    <a href="../operations/delete_account.php"><button>Delete Account</button></a>
+    <a href="../operations/logout.php"><button>Log out</button></a>
+    <a href="change_password.php"><button>Change password</button></a>
+</div>
+
+</body>
+</html>
