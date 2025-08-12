f7ddad0d1195dc835988e7055999515b978ebd6f
Hossein <hosseinyadegarnia1391@gmail.com>
2025-08-12 16:16:10 +0330

    Add delete account feature

--- operations/delete_account.php
+++ operations/delete_account.php
@@ -1,1 +1,14 @@
 <?php
+include "../database/database.php";
+global $conn;
+
+session_start();
+
+$username = $_SESSION['username'];
+$sql = "DELETE FROM users WHERE username='$username'";
+
+$result = mysqli_query($conn, $sql);
+
+session_destroy();
+header("location: ../index.php");
+exit;
